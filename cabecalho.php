ad97d56d916dac3f1ed57119b58703dc1713bd15
Jéssica Coutinho <jessicamaiara.coutinho@gmail.com>
2022-09-18 14:39:22 -0300

    Criação da minha Landing Page.

---
+++ cabecalho.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Lizzie Evans</title>
+</head>
+<body>
+
+    <header class="cabecalho navbar navbar-expand-lg">
+        <div class="container-fluid">
+        <div class="navbar-brand">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            
+                <nav class="navbar-nav">
+                <a class="nav-link" href="">HOME</a>
+                <a class="nav-link" href="">BLOG</a>
+                <a class="nav-link" href="">OBRAS</a>
+                <a class="nav-link" href="">CONTATOS</a>
+                </nav>
+            </div>
+        </div>
+        </div>
+    </header>
+
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
